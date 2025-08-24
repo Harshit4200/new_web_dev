05b84777681bce6fbcd964daf082d23b887e8c13
Harshit4200 <harshitjain2466@gmail.com>
2025-08-25 00:49:16 +0530

    new practice mini task of dom manipulation

---
+++ start/index.js
@@ -0,0 +1,16 @@
+
+function theme_toggler(){
+    document.body.classList.toggle("dark_mode");
+}
+// function popup(){
+//     document.body.style.position="fixed";
+//     document.querySelector(".modal").classList.add("popup-clicked");
+//     document.body.addEventListener("click",()=>{
+//     close();
+// })
+// }
+// function close(){
+//     document.querySelector(".modal").classList.remove("popup-clicked");
+//     document.body.style.position="static";
+
+// }
